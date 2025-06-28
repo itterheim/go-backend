07803293ffc63785913482352174e9e3689916f2
Martin Itterheim <itterheim.martin@gmail.com>
2025-06-28 19:42:22 +0200

    feat: main - start server\

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"backend/internal/config"
+	"backend/internal/db"
+	"backend/internal/router"
+	"backend/pkg/middleware"
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
-	start := time.Now()
+	startTime := time.Now()
 	defer func() {
-		elapsed := time.Since(start)
-		fmt.Println("Done in", elapsed.String())
+		endTime := time.Since(startTime)
+		fmt.Println("Done in:", endTime.Seconds(), "s")
 	}()
 
-	fmt.Println("API")
+	cfg, err := config.LoadConfig("./config/")
+	if err != nil {
+		fmt.Println("Error loading config:", err)
+		return
+	}
+
+	conn, err := db.ConnectPgx(cfg)
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+	defer conn.Close()
+
+	mux := router.NewRouter(conn, &cfg.Auth)
+
+	router := middleware.LoggingMiddleware(mux)
+
+	fmt.Println("Starting server on port", cfg.Server.Port)
+	err = http.ListenAndServe(":"+cfg.Server.Port, router)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
 }
