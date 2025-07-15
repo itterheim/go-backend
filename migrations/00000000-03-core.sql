7181da0f8b6dbf11fb706dc56677b780c0a8d29e
Martin Itterheim <itterheim.martin@gmail.com>
2025-07-15 20:00:12 +0200

    fix: remove actions from migrations

--- migrations/00000000-03-core.sql
+++ migrations/00000000-03-core.sql
@@ -7,6 +7,7 @@ CREATE TABLE events (
     status VARCHAR(20) NOT NULL,
     tags TEXT[],
     note TEXT,
+    reference TEXT,
     provider_id BIGINT,
     user_id BIGINT NOT NULL
 );
@@ -16,25 +17,9 @@ CREATE INDEX events_timestamp_idx ON events (timestamp);
 CREATE INDEX events_until_idx ON events (until);
 CREATE INDEX events_status_idx ON events (status);
 CREATE INDEX events_tags_idx ON events USING GIN(tags);
+CREATE INDEX events_reference_idx ON events (reference);
 CREATE INDEX events_user_id_idx ON events (user_id);
 CREATE INDEX events_provider_id_idx ON events (provider_id);
 
 ALTER TABLE events ADD CONSTRAINT fk_events_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL;
 ALTER TABLE events ADD CONSTRAINT fk_events_provider_id FOREIGN KEY (provider_id) REFERENCES providers (id) ON DELETE SET NULL;
-
--- actions
-CREATE TABLE actions (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    event_id BIGINT NOT NULL,
-    reference VARCHAR(63),
-    reference_id BIGINT,
-    tags TEXT[],
-    note TEXT
-);
-
-CREATE INDEX actions_event_id_idx ON actions (event_id);
-CREATE INDEX actions_reference_idx ON actions (reference);
-CREATE INDEX actions_reference_id_idx ON actions (reference_id);
-CREATE INDEX actions_tags_idx ON actions USING GIN(tags);
-
-ALTER TABLE actions ADD CONSTRAINT fk_actions_event_id FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE SET NULL;
